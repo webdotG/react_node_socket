c66be0f5fb91c3dff1e08799404d3356a72c00e8
Grant Kirill <webdotg.inbox@gmail.com>
2023-05-30 13:38:30 +0300

    fix cors

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const express = require('express')
-
+var cors = require('cors')
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {cors: {origin: '*'}})
 
-app.get('/test', function (request, response){
+app.use(cors())
+
+const testData = {
+		user: '',
+		data : [
+				{
+						data: 'some data',
+						data2: 'some data',
+				},],
+		time : '00:00'
+}
+
+const departmens = new Map ()
+
+app.get('/test',  (request, response) => {
 		console.log('get query done')
-		response.send('ответ получен')
+		response.json(testData)
+		console.log(testData)
 })
 
-app.listen(2525)
+io.on('connection', (socket) =>{
+		console.log('user connected :', socket.id)
+})
+
+server.listen(1111, (error) => {
+		if (error) {
+				throw Error(error)
+		}
+		console.log('server working')
+})
--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import React from "react";
+import io from "socket.io-client"
+
+const socket = io('http://localhost:1111')
 
 function App() {
 		return (
 				<>
+						<h1>websocket</h1>
 				</>
 		);
 }
